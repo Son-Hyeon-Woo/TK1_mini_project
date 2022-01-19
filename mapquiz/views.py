8d447093c49b254b824a1b2353b8b9c35f4dbe3c
sonhw <ths1611@naver.com>
2022-01-19 23:17:39 +0900

    0119 quiz 살짝 구현해보고, css적용 맛보기

--- mapquiz/views.py
+++ mapquiz/views.py
@@ -1,3 +1,19 @@
+from platform import java_ver
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from .models import Place
+import random
+def quiz(request):
+    place_list = Place.objects.all()
+    num = random.randrange(0,len(place_list))
+    answer = place_list[num]
+
+
+    return render(
+        request,
+        'mapquiz/quiz.html',
+        {'data': place_list,
+         'ans':answer,}
+    )
 
 # Create your views here.
