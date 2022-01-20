d884e4b3656e4133f33749bae1fcafbf4980aee2
Bae <qoru588@naver.com>
2022-01-21 00:18:33 +0900

    gyeong yong login logout hi

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('map/', include('mapquiz.urls')),
+    path('member/', include('member.urls')),
 ]
