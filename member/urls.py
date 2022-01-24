586c9ca22660e5b6683ee8aeda29011dc152fa6f
Bae <qoru588@naver.com>
2022-01-24 10:56:20 +0900

    a

--- member/urls.py
+++ member/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
+from django.contrib.auth import views
 app_name = 'member'
 
 urlpatterns = [
@@ -9,4 +9,12 @@ urlpatterns = [
             template_name='member/login.html'),
         name='login'
     ),
+    path('logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout'
+    ),
+    path('signup/', views.signup, name='signup'),
+    path('signup/custom/', views.signup_custom, name='signup_custom'),
+    path('login/custom/', views.login_custom, name='login_custom'),
+    path('logout/custom/', views.logout_custom, name='logout_custom'),
 ]
